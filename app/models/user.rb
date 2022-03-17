d5c36bf43d15e761ab665a9265e093b79d8523d7
A-Maruyama-dmm <com.mac.dm@gmail.com>
2022-03-17 08:06:10 +0000

    emailバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  
+   
   
   # 検索方法分岐
   def self.search_for(content, method)
@@ -26,7 +26,7 @@ class User < ApplicationRecord
   validates :age,  presence: true
   validates :sauna_years,  presence: true
   validates :number_of_sets,  presence: true
-  validates :email, length: {maximum: 20, minimum: 2}
+  validates :email, {presence: true, uniqueness: true}
 
 end
 
